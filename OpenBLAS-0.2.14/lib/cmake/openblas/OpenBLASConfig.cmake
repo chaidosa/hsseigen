db95810e63121dc9b7a770598e82d9cfe3270cb8
Pritesh <pritesh.vermaa@gmail.com>
2021-10-29 12:37:04 +0530

    removed files

--- OpenBLAS-0.2.14/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.14")
-SET(OpenBLAS_INCLUDE_DIRS /home/min/a/hegden/Research/DNN/Caffe/OpenBLAS-0.2.14/install/include)
-SET(OpenBLAS_LIBRARIES /home/min/a/hegden/Research/DNN/Caffe/OpenBLAS-0.2.14/install/lib/libopenblas.so)

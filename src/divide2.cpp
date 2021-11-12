0c3ed0c11528c08d6c059e5b6d60e7368b785ceb
priteshvermaa <pritesh0797@gmail.com>
2021-11-12 11:42:45 +0530

    adding file

---
+++ src/divide2.cpp
@@ -0,0 +1,12 @@
+#include "test_mat2hsssym.h"
+#include "BinTree.h"
+#include "divide2.h"
+
+DVD* divide2(tHSSMat *A, BinTree *bt,int* m, int mSize){
+
+int n = bt->GetNumNodes();
+//int desc = bt->GetDescendents();
+
+
+
+}

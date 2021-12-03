ad58be892e0490ff6f8cae9fa21bcba22650d2b6
Nikhil Hegde <nikhilh@iitdh.ac.in>
2021-12-04 03:05:21 +0530

    rewriting compr by considering rk (par) parameter

--- src/compr_new.cpp
+++ src/compr_new.cpp
@@ -17,80 +17,82 @@ extern "C"
 Q is the output parameter computing Ui, R is the output parameter computing Ri */
 void compr_new(double* A, std::pair<int, int>aSize, double** Q, std::pair<int, int>& qSize, double** R, std::pair<int, int>& rSize, char const* tol, double par)
 {
-    int rk = std::min(aSize.first,aSize.second);
-    double *S    = new double[rk];
+    int minMN = std::min(aSize.first,aSize.second);
+    double *S    = new double[minMN];
     double *U    = new double[aSize.first*(aSize.first)];
-    double *V    = new double[aSize.second*(aSize.second)];
-    double *W    = new double[aSize.first*(aSize.second)];
-   
+    double *VT   = new double[aSize.second*(aSize.second)];
+    double *W	 = new double[minMN-1];
+
+    //Note: m = aSize.first, n = aSize.second, lda = aSize.second, ldu = aSize.first, ldvt = aSize.second, info;
     //calling lapack routine for SVD singular value decomposition
-    LAPACKE_dgesvd(LAPACK_ROW_MAJOR,'A','A',aSize.first,aSize.second,A,aSize.second,S,U,aSize.first,V,aSize.second,W);    
-   
-    double *tempU = new double[aSize.first*rk];
-    //for(int row = 0; row<aSize.first; row++)
-    //    memcpy(tempU+row*rk,U+row*aSize.first,sizeof(double)*rk);
-    for(int row =0 ;row<aSize.first; row++){
-        for(int col=0; col<rk; col++){
-            tempU[col+(row*rk)] = U[col+(row*(aSize.first))];
-        }
-    }
-
-    *Q = tempU;
-    tempU = NULL;
-    qSize = {aSize.first,rk};
-
-    double *tempV = new double[rk*(aSize.second)];
-    double *temp = new double[rk*(aSize.second)];
-    //copying from V
-    for(int i=0;i<rk;i++){
-        for(int j=0;j<aSize.second;j++){
-            tempV[j+(i*(aSize.second))]=V[j+i*(aSize.second)];
-        }
-    }
-
-    //copying from S
-    double *tempS = new double[rk*rk];
-
-    for(int i=0;i<rk;i++){
-        for(int j=0;j<rk;j++){
-            tempS[j+i*rk] = 0;
-            if(i==j){
-                tempS[j+i*rk]=S[i];
-            }
-        }
-    }
-
- //  double* r1 = S;
- //   double* r2 = V;
-    
- //  S = tempS;
-  //  V = tempV;
-
-    //R = R*V'  Unlike Matlab, In this lapack routine V returned is already transposed so we don't need to transpose again.
-    for(int i=0;i<rk;i++)
-    {
-        for(int j=0;j<aSize.second;j++)
-        {
-            temp[j+i*aSize.second]=0;
-            for(int k=0;k<rk;k++)
-            {
-                temp[j+i*(aSize.second)]+=tempS[k+i*rk]*(tempV[j+k*(aSize.second)]);
-            }
-        }
-    }
-    
-    *R    = temp;
-    temp  = NULL;   
-    rSize = {rk,aSize.second};
-    delete [] S;
+    int info = LAPACKE_dgesvd(LAPACK_ROW_MAJOR,'A','A',aSize.first,aSize.second,A,aSize.second,S,U,aSize.first,VT,aSize.second,W);    
+   if(info > 0) {
+   	printf("ERROR: LAPACKE_dgesvd failed to converge\n");
+	exit(1);
+   }
     delete [] W;
-    delete [] V;
-    delete [] U;
-    delete [] tempS;
-    delete [] tempV;
-  //  delete [] r1;
-  //  delete [] r2;
     
-  
+    //To know the size of U, read On Return for u (ldu by atleast m): https://www.ibm.com/docs/en/essl/6.2?topic=llss-sgesvd-dgesvd-cgesvd-zgesvd-sgesdd-dgesdd-cgesdd-zgesdd-singular-value-decomposition-general-matrix
+    *Q=U;
+    qSize = {aSize.first,aSize.first}; 
 
+    //find the index i in S where S[i] > par*S[0])
+    double thresh=par*S[0];
+    int rk=0; 
+    for(rk=1;rk<minMN;rk++) {
+	if(S[rk] <= thresh)
+		break;
+    }
+    if(rk != aSize.first) {
+    	//pick only the first rk columns of U. U is of size aSize.first x aSize.first (ROW_MAJOR).
+	//When you look at U (now aSize.first x rk) the columns represent left singular vectors.
+    	double *tempU = new double[aSize.first*rk];
+	for(int i=0;i<aSize.first;i++)
+		memcpy(tempU+i*rk,U+i*aSize.first,sizeof(double)*rk);
+	*Q=tempU;
+	qSize = {aSize.first,rk}; 
+	delete [] U;
+
+    	//pick only the first rk rows of VT. VT is of size aSize.second x aSize.second (ROW_MAJOR)
+	//When you look at VT (now rk x aSize.second) the rows represent right singular vectors.
+	double * tempVT = NULL;
+	if( rk != aSize.second) {
+		tempVT = new double[rk*aSize.second];
+		for(int i=0;i<rk;i++)
+			memcpy(tempVT+i*aSize.second,VT+i*aSize.second,sizeof(double)*aSize.second);
+    		delete [] VT;
+	}
+	else
+		tempVT = VT;
+	
+	//pick only the first rk singular values from S. Multiply S (representing a diagonal matrix of size rk x rk ) and tempVT ( matrix of size rk x aSize.second)
+	for(int i=0;i<rk;i++)
+		for(int j=0;j<aSize.second;j++)
+			tempVT[i*aSize.second+j]=S[i]*tempVT[i*aSize.second+j];
+	*R=tempVT;
+    	rSize = {rk,aSize.second};
+    }
+    else {
+	    //Output parameter Q is already set earlier. Compute output parameter R.
+	    //R is computed as S*VT (S represents a diagonal matrix of size aSize.first x aSize.second of which only the min(aSize.first, aSize.second) diagonal elements are actually non-zeros. VT represents a matrix of size (aSize.second x aSize.second)
+	    double *tempR = new double[aSize.first * aSize.second];
+	    memset(tempR, 0, sizeof(double)*aSize.first*aSize.second);
+	    if(aSize.first <= aSize.second) {
+		    //rectangular matrix with number of rows smaller than number of cols. 
+		    for(int i=0;i<aSize.first;i++)
+			for(int j=0;j<aSize.second;j++)
+				tempR[i*aSize.second+j]=S[i]*VT[i*aSize.second+j];
+	    }
+	    else {
+		    //rectangular matrix with number of cols smaller than number rows. 
+		    for(int i=0;(i<aSize.first) && (i<aSize.second);i++)
+			for(int j=0;j<aSize.second;j++)
+				tempR[i*aSize.second+j]=S[j]*VT[i*aSize.second+j];
+	    }
+	    *R = tempR;
+    	    rSize = {aSize.first,aSize.second};
+	    delete [] VT;
+    }
+
+    delete [] S;
 }

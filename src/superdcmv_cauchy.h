40dc7d037503105491af2575f640a868017545b6
priteshiitdh <pritesh.verma@iitdh.ac.in>
2021-12-23 01:35:35 +0530

    adding header

--- src/superdcmv_cauchy.h
+++ src/superdcmv_cauchy.h
@@ -1,6 +1,7 @@
 #ifndef SUPERDCMV_CAUCHY_H
 #define SUPERDCMV_CAUCHY_H
-
+#include <iostream>
+#include <utility>
 void superdcmv_desc(double **Q,std::pair<int, int>*qSize, double *X,std::pair<int, int>xSize,int ifTrans,double N = 1024);
 
 

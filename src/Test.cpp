919166417b7069316b6bf123299807c778908dc2
priteshvermaa <pritesh0797@gmail.com>
2022-05-02 15:05:14 +0530

    adding files for testing

--- src/Test.cpp
+++ src/Test.cpp
@@ -7,140 +7,55 @@
 #include "QR.h"
 #include "superDC.h"
 #include "band2hss.h"
-#include "Generators.h"
 #include <sys/time.h>
 #include<assert.h>
-#include <fstream>
 
-int main()
+char* testFile="Band5.txt"; // Pritesh: Band5.txt is there in Pnk system inside /hsseigen/ directory
+
+int main(int argc, char* argv[])
 {
-	int n=4096, band_width=2;
+	int n=32768, band_width=2;
 	int r=256;
-	int MorB = 1; // to select which routine to use Band2HSS (2) or Mat2HSSsymm (1).
-	int w = 0;	//  band of the matrix, can be changes according to the requirement.
+	int MorB = 2; // to select which routine to use Band2HSS (2) or Mat2HSSsymm (1).
+	int w = 2;	//  band of the matrix, can be changes according to the requirement.
+	int nProc = 1; // no of procs to use
+	if(argc!=7){
+		printf("Usage: ./Test <filename> <matrix_size> <diagblock_size> <Band2HSS or Mat2HSSsym> <bandwidth>\n");
+		
+	}else{
+		testFile=argv[1];
+		n=atoi(argv[2]);
+		r=atoi(argv[3]);
+		MorB = atoi(argv[4]);
+		w = atoi(argv[5]);
+		nProc = atoi(argv[6]);
+	}
 
 	BinTree* bt=NULL;
+	double * A=NULL;
+	//create a banded matrix
+	int status = MakeBand(n,band_width,&A);
+	if(status) 
+		exit(-1);
 	
 	
 	int numNodes = 0;
 	
+
 	/*(you can either reuse the tree created earlier or let the call to NPart create a new tree based on the size of the partition specified.
 	 * Arguments of NPart: n is the number of rows/columns in an input matrix. r is the number of rows in a partition (horizontal) of the matrix 
 	 * Number of leaves = n/r. Num nodes in the tree = num leaves* 2 - 1*/
 	int *m=NULL;
 	int mSize;
 	NPart(n, r, &bt, &m, mSize, numNodes);
-	int N = bt->numNodes;
-	GEN *hss = new GEN();
-	double **D = new double*[bt->numNodes];
-	std::pair<int, int>*dSizes = new std::pair<int, int>[N];
 
-	double **U = new double*[bt->numNodes];
-	std::pair<int, int>*uSizes = new std::pair<int, int>[N];
 
-	double **R = new double*[bt->numNodes];
-	std::pair<int, int>*rSizes = new std::pair<int, int>[N];
 
-	double **B = new double*[bt->numNodes];
-	std::pair<int, int>*bSizes = new std::pair<int, int>[N];
+	GEN *hss = HssGenerators(A, n*n, bt, m , mSize, w, MorB);
 	
-	std::ifstream ifD ("genD4.txt", std::ifstream::in);
-	if(!ifD.is_open()){
-		cout << "Error in opening test file"<<endl;
-		assert(false);
-	}
-	while(!ifD.eof()){
-		int i, row, col;
-		ifD >> i;
-		ifD >> row;
-		ifD >> col;
-		dSizes[i-1] = make_pair(row, col);
-		D[i-1] = new double[row * col];
-
-		for(int r = 0; r < row; r++){
-			for(int c = 0; c < col; c++){
-				ifD >> D[i-1][c + r*col];
-			}
-		}	
-	}
 	
-	 std::ifstream ifU ("genU4.txt", std::ifstream::in);
-        if(!ifU.is_open()){
-                cout << "Error in opening test file"<<endl;
-                assert(false);
-        }
-        while(!ifU.eof()){
-                int i, row, col;
-                ifU >> i;
-                ifU >> row;
-                ifU >> col;
-                uSizes[i-1] = make_pair(row, col);
-                U[i-1] = new double[row * col];
-
-                for(int r = 0; r < row; r++){
-                        for(int c = 0; c < col; c++){
-                                ifU >> U[i-1][c + r*col];
-                        }
-                }
-        }
-      	
-	 std::ifstream ifR ("genR4.txt", std::ifstream::in);
-        if(!ifR.is_open()){
-                cout << "Error in opening test file"<<endl;
-                assert(false);
-        }
-        while(!ifR.eof()){
-                int i, row, col;
-                ifR >> i;
-                ifR >> row;
-                ifR >> col;
-                rSizes[i-1] = make_pair(row, col);
-                R[i-1] = new double[row * col];
-
-                for(int r = 0; r < row; r++){
-                        for(int c = 0; c < col; c++){
-                                ifR >> R[i-1][c + r*col];
-                        }
-                }
-        }
-
-
-
-	 std::ifstream ifB ("genB4.txt", std::ifstream::in);
-        if(!ifB.is_open()){
-                cout << "Error in opening test file"<<endl;
-                assert(false);
-        }
-        while(!ifB.eof()){
-                int i, row, col;
-                ifB >> i;
-                ifB >> row;
-                ifB >> col;
-                bSizes[i-1] = make_pair(row, col);
-                B[i-1] = new double[row * col];
-
-                for(int r = 0; r < row; r++){
-                        for(int c = 0; c < col; c++){
-                                ifB >> B[i-1][c + r*col];
-                        }
-                }
-        }
-
-	hss->D = D;
-	hss->dSizes = dSizes;
-
-	hss->B = B;
-        hss->bSizes = bSizes;
-
-	hss->R = R;
-        hss->rSizes = rSizes;
-
-	hss->U = U;
-        hss->uSizes = uSizes;
-
-	SDC* res = superDC(hss, bt, m, mSize, 1);
-///	res->~SDC();
-//	delete[] m;
+	SDC* res = superDC(hss, bt, m, mSize, nProc);
+	delete[] m;
 	
 	return 0;
 }
